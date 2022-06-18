d6c71bc53c59f423fcb47acdcd2b9e9bc83e6a6b
Aviya Uzan <aviyauzan@gmail.com>
2022-06-18 21:05:55 +0300

    commit

--- js/services/util.service.js
+++ js/services/util.service.js
@@ -1,6 +1,23 @@
 'use strict'
 
-function toggleMenu(elBtn) {
+
+function toggleMenu() {
     document.body.classList.toggle('menu-open');
-    // document.body.classList.contains('menu-open') ? elBtn.innerText = 'X' : elBtn.innerText = '☰';
+}
+
+function showModal() {
+   var modal = document.querySelector('.modal')
+   modal.classList.toggle('hidden'); 
+}
+
+function closeModal() {
+    var modal = document.querySelector('.modal')
+    modal.classList.add('slide-out-elliptic-top-bck');
+    setTimeout(removeAnime, 1000)
+}
+
+function removeAnime() {
+    var modal = document.querySelector('.modal')
+    modal.classList.toggle('hidden');
+    modal.classList.remove('slide-out-elliptic-top-bck')
 }
